cd3a912e2060d84d27e37c368e56c21a54d8a8b7
lgdz <xiongwq@foxmail.com>
2023-05-10 09:20:59 +0800

    -

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lgdzz/vingo-utils
+
+go 1.19
+
+require github.com/google/uuid v1.3.0
---
+++ vingo/random.go
@@ -0,0 +1,53 @@
+package vingo
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func GetUUID() string {
+	return uuid.NewString()
+}
+
+func GetRandomString(size int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	var result []byte
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < size; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+func RandomString(length int) string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func RandomNumber(length int) string {
+	digits := []rune("0123456789")
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = digits[rand.Intn(len(digits))]
+	}
+	return string(b)
+}
+
+func OrderNo(length int, check func(string)) string {
+	now := time.Now()
+	orderNo := fmt.Sprintf("%v%v", now.Format("20060102150405"), RandomNumber(length-14))
+	if check != nil {
+		check(orderNo)
+	}
+	return strings.ToUpper(orderNo)
+}
