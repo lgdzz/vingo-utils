a7c84f2767b580e4b0f23104ef7a19e8209b9f1f
lgdz <xiongwq@foxmail.com>
2023-05-15 11:19:45 +0800

    -

---
+++ vingo/exception.go
@@ -0,0 +1,27 @@
+package vingo
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// 异常处理
+func ExceptionHandler(c *gin.Context) {
+	context := &Context{c}
+
+	defer func() {
+		if err := recover(); err != nil {
+			switch t := err.(type) {
+			case string:
+				context.Response(&ResponseData{Message: t, Status: 200, Error: 1, ErrorType: "业务错误"})
+			case *DbException:
+				context.Response(&ResponseData{Message: t.Message, Status: 200, Error: 1, ErrorType: "数据库错误"})
+			case *AuthException:
+				context.Response(&ResponseData{Message: t.Message, Status: 401, Error: 1})
+			default:
+				context.Response(&ResponseData{Message: t.(error).Error(), Status: 200, Error: 1, ErrorType: "异常错误"})
+			}
+			c.Abort()
+		}
+	}()
+	c.Next()
+}
