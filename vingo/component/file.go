6c70840600736f4a4d2921ed3e54db4aa8755a2e
lgdz <xiongwq@foxmail.com>
2023-11-03 11:11:38 +0800

    -

---
+++ vingo/component/file.go
@@ -0,0 +1,50 @@
+package component
+
+import (
+	"github.com/lgdzz/vingo-utils/vingo"
+	"github.com/lgdzz/vingo-utils/vingo/db/mysql"
+	"github.com/lgdzz/vingo-utils/vingo/db/page"
+)
+
+// File 文件
+type File struct {
+	ID        uint             `gorm:"primaryKey;column:id" json:"id"`
+	CID       uint             `gorm:"column:c_id" json:"cId"`          // 分类ID，预留
+	Channel   string           `gorm:"column:channel" json:"channel"`   // 渠道
+	OrgID     uint             `gorm:"column:org_id" json:"orgId"`      // 关联组织ID
+	FromID    string           `gorm:"column:from_id" json:"fromId"`    // 来源ID
+	Type      string           `gorm:"column:type" json:"type"`         // 文件类型
+	Filename  string           `gorm:"column:filename" json:"filename"` // 资源名称
+	Filepath  string           `gorm:"column:filepath" json:"filepath"` // 资源路径
+	Filesize  uint             `gorm:"column:filesize" json:"filesize"` // 文件大小
+	Mimetype  string           `gorm:"column:mimetype" json:"mimetype"`
+	Extension string           `gorm:"column:extension" json:"extension"` // 文件后缀
+	Extra     string           `gorm:"column:extra" json:"extra"`         // 附件属性
+	CreatedAt *vingo.LocalTime `gorm:"column:created_at;" json:"createdAt"`
+	UpdatedAt *vingo.LocalTime `gorm:"column:updated_at" json:"updatedAt"`
+}
+
+// TableName get sql table name.获取数据库表名
+func (m *File) TableName() string {
+	return "file"
+}
+
+// 文件记录保存
+func FileRecordSave(c *vingo.Context) {
+	body := vingo.GetRequestBody[File](c)
+	body.OrgID = c.GetOrgId()
+	body.Channel = "backstage"
+	mysql.Create(&body)
+	c.ResponseSuccess()
+}
+
+// 文件记录列表
+func FileRecordList(c *vingo.Context) {
+	query := vingo.GetRequestQuery[page.Limit](c)
+	db := mysql.Table("file").Where("org_id=?", c.GetOrgId())
+	result := page.New[File](db, page.Option{
+		Limit: page.Limit{Page: query.Page, Size: query.Size},
+		Order: page.OrderDefault(&page.Order{}),
+	}, nil)
+	c.ResponseBody(result)
+}
--- vingo/oss/qiniu/qiniu.go
+++ vingo/oss/qiniu/qiniu.go
@@ -1,10 +1,20 @@
 package aliyun
 
 import (
+	"fmt"
+	"github.com/lgdzz/vingo-utils/vingo/db/redis"
 	"github.com/lgdzz/vingo-utils/vingo/oss"
+	"github.com/qiniu/go-sdk/v7/auth/qbox"
+	"github.com/qiniu/go-sdk/v7/storage"
+	"time"
 )
 
 type Config struct {
+	AccessKey string
+	SecretKey string
+	Bucket    string
+	Expires   uint64
+	Cache     bool
 }
 
 type ClientApi struct {
@@ -19,6 +29,39 @@ func InitClient(config Config) (api ClientApi) {
 	api.Client = ""
 
 	return api
+}
+
+// 上传签名
+func (s *ClientApi) Sign() (token string) {
+	key := fmt.Sprintf("qiniu_%v", s.Config.AccessKey)
+	putPolicy := storage.PutPolicy{
+		Scope: s.Config.Bucket,
+	}
+
+	// 签名有效期
+	if s.Config.Expires == 0 {
+		putPolicy.Expires = 600 // 默认10分钟有效期
+	} else {
+		putPolicy.Expires = s.Config.Expires
+	}
+
+	if s.Config.Cache {
+		tokenPoint := redis.Get[string](key)
+		// 从缓存中读取凭证
+		if tokenPoint != nil {
+			token = *tokenPoint
+			return
+		}
+	}
+
+	mac := qbox.NewMac(s.Config.AccessKey, s.Config.SecretKey)
+	token = putPolicy.UploadToken(mac)
+
+	if s.Config.Cache {
+		// 缓存提前100秒失效
+		redis.Set(key, token, time.Duration(putPolicy.Expires-100)*time.Second)
+	}
+	return
 }
 
 // todo
