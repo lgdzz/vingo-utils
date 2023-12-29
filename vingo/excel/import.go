5afed7f3a815e31e58f46a0a84e05a428cb227fc
lgdz <xiongwq@foxmail.com>
2023-12-29 16:47:12 +0800

    -

--- vingo/excel/import.go
+++ vingo/excel/import.go
@@ -19,24 +19,58 @@ import (
 //				Phone: cells[1].String(),
 //			})
 //		})
-func ReadData(excelPath string, rowFunc func([]*xlsx.Cell)) {
+//
+// Deprecated: This function is no longer recommended for use.
+// Suggested: Please use ReadDatas() instead.
+func ReadData(excelPath string, rowFunc ...func([]*xlsx.Cell)) {
 	// 打开Excel文件
 	xlFile, err := xlsx.OpenFile(excelPath)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// 假设文件中只有一个工作表
-	sheet := xlFile.Sheets[0]
+	for sheetIndex := range rowFunc {
+		sheet := xlFile.Sheets[sheetIndex]
 
-	// 遍历每一行（忽略表头）
-	for _, row := range sheet.Rows[1:] {
-		// 读取单元格数据
-		rowFunc(row.Cells)
+		// 遍历每一行（忽略表头）
+		for _, row := range sheet.Rows[1:] {
+			// 读取单元格数据
+			rowFunc[sheetIndex](row.Cells)
+		}
 	}
 
 	// 删除Excel文件
 	err = os.Remove(excelPath)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func ReadDatas(file string, startRowIndex int, rowFunc ...func([]*xlsx.Cell)) {
+	// 打开Excel文件
+	xlFile, err := xlsx.OpenFile(file)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for sheetIndex, f := range rowFunc {
+		currentIndex := sheetIndex
+		if currentIndex < len(xlFile.Sheets) {
+			sheet := xlFile.Sheets[sheetIndex]
+			for _, row := range sheet.Rows[startRowIndex:] {
+				if len(row.Cells) == 0 {
+					continue
+				} else if row.Cells[0].String() == "" {
+					// 如果第一列单元格为空，则认定该行为空数据
+					continue
+				}
+				f(row.Cells)
+			}
+		}
+	}
+
+	// 删除Excel文件
+	err = os.Remove(file)
 	if err != nil {
 		panic(err.Error())
 	}
